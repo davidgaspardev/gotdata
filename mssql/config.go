03723bf665d7fdb5effe2da3a42f41726bc9a2c3
David Gaspar <davidgaspar.dev@gmail.com>
2022-09-10 10:13:03 -0300

    chore: add connection with the SQL Server

---
+++ mssql/config.go
@@ -0,0 +1,63 @@
+package mssql
+
+import (
+	"database/sql"
+	"fmt"
+	"net/url"
+	"os"
+)
+
+// Get all the data to make tha connection
+func getConnectionData() (username string, password string, host string, port string, instance string, database string) {
+	username = os.Getenv("MSSQL_USERNAME")
+	password = os.Getenv("MSSQL_PASSWORD")
+	host = os.Getenv("MSSQL_HOST")
+	port = os.Getenv("MSSQL_PORT")
+	instance = os.Getenv("MSSQL_INSTANCE")
+	database = os.Getenv("MSSQL_DATABASE")
+
+	return
+}
+
+// Check if exists connection
+func hasConnectionData() bool {
+	var username,
+		password,
+		host,
+		port,
+		instance,
+		database = getConnectionData()
+
+	return (username != "" &&
+		password != "" &&
+		host != "" &&
+		port != "" &&
+		instance != "" &&
+		database != "")
+}
+
+func createConnectionUrl() string {
+	var username,
+		password,
+		host,
+		port,
+		instance,
+		database = getConnectionData()
+
+	query := url.Values{}
+	query.Add("database", database)
+
+	url := &url.URL{
+		Scheme:   "sqlserver",
+		User:     url.UserPassword(username, password),
+		Host:     fmt.Sprintf("%s:%s", host, port),
+		Path:     instance,
+		RawQuery: query.Encode(),
+	}
+
+	return url.String()
+}
+
+func configDatabaseConnection(db *sql.DB) {
+	db.SetMaxOpenConns(5)
+}
---
+++ mssql/mssql.go
@@ -0,0 +1,49 @@
+package mssql
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/davidgaspardev/golog"
+)
+
+type _DatabaseMssql struct {
+	db *sql.DB
+}
+
+var instance *_DatabaseMssql
+
+func init() {
+	// Check if has connection
+	if !hasConnectionData() {
+		err := fmt.Errorf("don't has connection data")
+		golog.Error("Mssql", err)
+		panic(err)
+	}
+
+	// Make connection to the database
+	db, err := connectDatabase()
+	if err != nil {
+		golog.Error("Mssql", err)
+		panic(err)
+	}
+
+	// Configure database connection for better performance
+	configDatabaseConnection(db)
+
+	// Alloc instance
+	instance = &_DatabaseMssql{
+		db: db,
+	}
+}
+
+func connectDatabase() (db *sql.DB, err error) {
+	// Create pool connection
+	db, err = sql.Open("sqlserver", createConnectionUrl())
+
+	return
+}
+
+func GetInstance() *_DatabaseMssql {
+	return instance
+}
